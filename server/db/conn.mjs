2975702c5d63d9b122db74971af44fcacd40f75a
lumixAr12 <akujuaobidaniel@gmail.com>
2023-10-18 17:25:16 +0100

    -first commit

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,20 +1,33 @@
 
-import { MongoClient } from "mongodb"
+// import { MongoClient } from "mongodb"
 
-const connectionString = process.env.ATLAS_URI;
-const client = new MongoClient(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const connectionString = process.env.ATLAS_URI;
+// const client = new MongoClient(connectionString, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-let dbConnection;
+// let dbConnection;
 
-module.exports = {
-  connectToServer: function (callback) {
-    // Implement Database connection
-  },
+// module.exports = {
+//   connectToServer: function (callback) {
+//     // Implement Database connection
+//   },
 
-  getDb: function () {
-    return dbConnection;
-  },
-};
+//   getDb: function () {
+//     return dbConnection;
+//   },
+// };
+
+import mongodb from "mongodb";
+const { MongoClient } = mongodb
+const connectionString = process.env.ATLAS_URI || "";
+const client = new MongoClient(connectionString, { useUnifiedTopology: true });
+let conn;
+try {
+  conn = await client.connect();
+} catch (e) {
+  console.error(e);
+}
+let db = conn.db("sample_training");
+export default db;
--- server/index.mjs
+++ server/index.mjs
@@ -1,4 +1,6 @@
 
+import "./loadEnv.mjs"
+import posts from "./routes/posts.mjs"      //importing the loadEnv
 import express from 'express'
 import cors from 'cors'
 
@@ -7,3 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
+// Load the /posts routes
+app.use("/posts", posts);
+
+
+//Listens to the PORT
+app.listen(PORT, () => {
+    console.log('server is running on',PORT)
+})
