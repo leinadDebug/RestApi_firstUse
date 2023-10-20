8eca2ae65cf53ff1bc1a463081db88b26f370433
leinadDebug <akujuaobidaniel@gmail.com>
2023-10-21 00:13:36 +0100

    --custom sidebar

--- app/src/Components/SideBar.jsx
+++ app/src/Components/SideBar.jsx
@@ -6,17 +6,17 @@ function SideBar() {
     <div className=" fixed  pr-14 flex flex-col py-10 text-lg gap-4  bg-amber-50 h-screen border border-l-2 border-amber-200  ">
       <Link
         to={"/"}
-        className="font-medium px-4 text-amber-500 rounded-r-xl py-1 hover:bg-amber-400 hover:text-white  ">
+        className="font-medium px-4 text-amber-500 rounded-r-xl py-1 hover:bg-amber-400 hover:text-white hover:duration-300 hover:border-b-4 border-b-amber-200 ">
         Home
       </Link>
       <Link
         to={"/archive"}
-        className="font-medium px-4 text-amber-500 rounded-r-xl py-1 hover:bg-amber-400 hover:text-white  ">
+        className="font-medium px-4 text-amber-500 rounded-r-xl py-1 hover:bg-amber-400 hover:text-white hover:duration-300 hover:border-b-4 border-l-amber-200 ">
         Achieve
       </Link>
       <Link
         to={"/post"}
-        className="font-medium px-4 text-amber-500 rounded-r-xl py-1 hover:bg-amber-400 hover:text-white  ">
+        className="font-medium px-4 text-amber-500 rounded-r-xl py-1 hover:bg-amber-400 hover:text-white hover:duration-300  hover:border-b-4 border-l-amber-200">
         New Post
       </Link>
     </div>
