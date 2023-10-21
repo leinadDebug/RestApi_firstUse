7ee3ddccded6b751f9b543a4682f92193feaee12
leinadDebug <akujuaobidaniel@gmail.com>
2023-10-21 07:56:08 +0100

    --Errorpage

--- app/src/Components/Error404.jsx
+++ app/src/Components/Error404.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Error404() {
   return (
-    <div class>
-      <h1 c>{`(> _ <)`}</h1>
+    <div>
+      <h1>{`(> _ <)`}</h1>
       <p>Unfortunately, this page doesnt exist</p>
     </div>
   );
